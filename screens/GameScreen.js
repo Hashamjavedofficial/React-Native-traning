38664679377d3657aa7608077babfd2c0a6e753d
Hasham Javed <hasham.hasham1@gmail.com>
2021-12-17 12:52:44 +0500

    lower and greater button working

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, Text, StyleSheet, Button, Alert} from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -20,14 +20,39 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomNumber(1, 100, userChoice),
   );
+  let currentLow = useRef(1);
+  let currentHigh = useRef(100);
+
+  const nextGuessHandler = direction => {
+    if (
+      (direction === 'lower' && currentGuess < userChoice) ||
+      (direction === 'greater' && currentGuess > userChoice)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        {text: 'Sorry!!', style: 'cancel'},
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomNumber(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess,
+    );
+    setCurrentGuess(nextNumber);
+  };
 
   return (
     <View style={styles.screen}>
       <Text>Opponent Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="Lower" />
-        <Button title="Greater" />
+        <Button title="Lower" onPress={() => nextGuessHandler('lower')} />
+        <Button title="Greater" onPress={() => nextGuessHandler('greater')} />
       </Card>
     </View>
   );
